008b7622a8a79c609825987fef41fe790444e5fb
toyotomi69 <100810978+toyotomi69@users.noreply.github.com>
2022-03-04 00:26:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -114,4 +114,21 @@ client.on('message', (channel, user, message, self) => {
         client.say(channel, `${thing}`);
         
     }
+    if(message == '!cas') {
+        
+        client.say(channel, `Kristova noho, ono už je ${currentTime()}`);
+        
+    }
+    function currentTime() {
+      let date = new Date(); 
+      let hh = date.getHours();
+      let mm = date.getMinutes();
+       hh = (hh < 10) ? "0" + hh : hh;
+       mm = (mm < 10) ? "0" + mm : mm;
+
+       let time = hh + ":" + mm + " " ;
+
+       
+      let t = setTimeout(function(){ currentTime() }, 1000);
+    }
 });
