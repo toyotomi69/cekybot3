d077a2a33e1e461967edc279b48815c785f10593
toyotomi69 <100810978+toyotomi69@users.noreply.github.com>
2022-03-04 23:50:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -120,8 +120,7 @@ client.on('message', (channel, user, message, self) => {
         
     }
     if(message == '!cas') {
-        const currentdate = new Date(); 
-        currentdate.setTimezone("Europe/Prague");
+        const currentdate = new Date().toLocaleString('en-US', { timeZone: 'Europe/Prague' });
                 var datetime = currentdate.getHours() + ":" + currentdate.getMinutes();
                 Date.prototype.timeNow = function () {
                     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
