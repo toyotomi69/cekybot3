e3f2d94c949115178bee78dc3332ae771d8c7bcb
toyotomi69 <100810978+toyotomi69@users.noreply.github.com>
2022-03-17 21:50:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ client.on('message', (channel, user, message, self) => {
 
 				client.say(
 					channel,
-					`Vojta právě dostal ${amountInGas.toLocaleString()} ${litr.toLocaleString()} benzínu :) dojel by tedy ${km}km`
+					`Vojta právě dostal ${amountInGas.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} ${litr..toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} benzínu :) dojel by tedy ${km}km`
 				)
 			}
 		}
